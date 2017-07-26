1a91dc7433792729950fa491a6844c907464b38c
Jari Pennanen <ciantic@oksidi.com>
2017-07-26 16:43:50 +0300

    ...

--- tests/test_oslosm/keymap.c
+++ tests/test_oslosm/keymap.c
@@ -16,10 +16,6 @@
 
 #include "quantum.h"
 
-// Don't rearrange keys as existing tests might rely on the order
-// Col2, Row 0 has to be KC_NO, because tests rely on it
-
-#define COMBO1 RSFT(LCTL(KC_O))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = {
@@ -35,17 +31,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,       KC_NO, KC_NO},
         {KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,       KC_NO, KC_NO},
         {KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,       KC_NO, KC_NO},
+    },
+    [2] = {
+        // 0      1        2      3        4        5        6       7            8      9
+        {KC_TRNS, KC_TRNS, KC_D,  KC_E,   KC_NO,   KC_NO,   KC_NO,  KC_NO,       KC_NO, KC_NO},
+        {KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,       KC_NO, KC_NO},
+        {KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,       KC_NO, KC_NO},
+        {KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,       KC_NO, KC_NO},
     }
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-    // if (record->event.pressed) {
-    //     switch(id) {
-    //     case 0:
-    //         return MACRO(D(LSFT), T(H), U(LSFT), T(E), T(L), T(L), T(O), T(SPACE), W(100), 
-    //         D(LSFT), T(W), U(LSFT), I(10), T(O), T(R), T(L), T(D), D(LSFT), T(1), U(LSFT), END);
-    //     }
-    // }
     return MACRO_NONE;
 };
 
--- tests/test_oslosm/test_osl.cpp
+++ tests/test_oslosm/test_osl.cpp
@@ -22,39 +22,39 @@ I think one shots should always work like this.
 
 // // OSL switches layer, but only after 200ms the tapping term
 // // This is unwanted behavior if tap toggling is not turned ons
-// TEST_F(OslTests, OslShouldSwitchLayerAfterTappingTerm) {
-//     TestDriver driver;
-//     layer_state = 0;
-//     EXPECT_EQ(0, layer_state);
+TEST_F(OslTests, OslShouldSwitchLayerAfterTappingTerm) {
+    TestDriver driver;
+    layer_state = 0;
+    EXPECT_EQ(0, layer_state);
 
-//     // OSL(1) DOWN
-//     press_key(0, 0);
-//     run_one_scan_loop();
-//     EXPECT_EQ(0, layer_state); // It has not changed yet
+    // OSL(1) DOWN
+    press_key(0, 0);
+    run_one_scan_loop();
+    EXPECT_EQ(0, layer_state); // It has not changed yet
 
-//     idle_for(200); // TAPPING_TERM
-//     EXPECT_EQ(2, layer_state); // It has now changed
-// }
+    idle_for(200); // TAPPING_TERM
+    EXPECT_EQ(2, layer_state); // It has now changed
+}
 
-// // OSL Down, then key should work
-// TEST_F(OslTests, OslDownThenKey) {
-//     TestDriver driver;
-//     layer_state = 0;
+// OSL Down, then key should work
+TEST_F(OslTests, OslDownThenKey) {
+    TestDriver driver;
+    layer_state = 0;
 
-//     // OSL(1) DOWN
-//     press_key(0, 0);
-//     run_one_scan_loop();
-//     idle_for(200); // TAPPING_TERM
+    // OSL(1) DOWN
+    press_key(0, 0);
+    run_one_scan_loop();
+    idle_for(200); // TAPPING_TERM
 
-//     // Layer 1, KC_A down
-//     press_key(2, 0);
-//     EXPECT_CALL(driver, send_keyboard_mock(KeyboardReport(KC_A)));
-//     run_one_scan_loop();
-// }
+    // Layer 1, KC_A down
+    press_key(2, 0);
+    EXPECT_CALL(driver, send_keyboard_mock(KeyboardReport(KC_A)));
+    run_one_scan_loop();
+}
 
 // OSL Down,Up then key should work
 TEST_F(OslTests, OslDownUpThenKey) {
-    TestDriver driver;
+      TestDriver driver;
     // layer_state = 0;
 
     // OSL(1) DOWN
@@ -63,13 +63,18 @@ TEST_F(OslTests, OslDownUpThenKey) {
 
     // OSL(1) UP
     release_key(0, 0);
+    EXPECT_CALL(driver, send_keyboard_mock(KeyboardReport())).Times(2);
     run_one_scan_loop();
+    testing::Mock::VerifyAndClearExpectations(&driver);
 
     EXPECT_EQ(2, layer_state); // It has now changed
     // run_one_scan_loop();
 
+    testing::InSequence s;
     // Layer 1, KC_A down
     press_key(2, 0);
+    EXPECT_CALL(driver, send_keyboard_mock(KeyboardReport())).Times(1);
     EXPECT_CALL(driver, send_keyboard_mock(KeyboardReport(KC_A)));
+    EXPECT_CALL(driver, send_keyboard_mock(KeyboardReport())).Times(3);
     run_one_scan_loop();
 }
